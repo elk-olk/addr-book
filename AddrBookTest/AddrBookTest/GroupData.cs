ba7c6083445e46a404781a62a093f1c9bd9bbfc1
Елена Омельчук <elena.omelchuk@gmail.com>
2018-03-31 14:53:28 +0300

    GroupData and AccountData classes added (task 3)

---
+++ AddrBookTest/AddrBookTest/GroupData.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAddressbookTests
+{
+     class GroupData
+    {
+        private string grName;
+        private string grHeader="";
+        private string grFooter="";
+
+        public GroupData(string grName)
+        {
+            this.grName = grName;
+        }
+
+
+        public GroupData(string grName, string grHeader, string grFooter)
+        {
+            this.grName = grName;
+            this.grHeader = grHeader;
+            this.grFooter = grFooter;
+        }
+
+
+        public string GrName
+        {
+            get
+            {
+                return grName;
+            }
+            set
+            {
+                grName = value;
+            }
+        }
+
+        public string GrHeader
+        {
+            get
+            {
+                return grHeader;
+            }
+            set
+            {
+                grHeader = value;
+            }
+        }
+
+        public string GrFooter
+        {
+            get
+            {
+                return grFooter;
+            }
+            set
+            {
+                grFooter = value;
+            }
+        }
+
+    }
+}
